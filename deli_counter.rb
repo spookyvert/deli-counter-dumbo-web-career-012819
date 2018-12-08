899b28f5bef2cda20754639c43da036bf437a39b
Shemar Dacosta <dshemar6@gmail.com>
2018-12-08 04:47:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def line(katz_deli)
+  k = katz_deli
+  if k.empty?
+    "The line is currently empty"
+  else
+    message = "The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
+  
+end
